b34fc6ce35e1c9899406aee706ff493268bc8897
Héctor Vega <linnk99@gmail.com>
2021-11-05 00:57:04 -0500

    Manejo de alerts

---
+++ madison_alert_popup.py
@@ -0,0 +1,36 @@
+import unittest
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+class UsingUnnittest(unittest.TestCase):
+
+    def setUp(self):
+        s = Service('./chromedriver')
+        self.driver = webdriver.Chrome(service = s)
+        driver = self.driver
+        driver.get('http://demo-store.seleniumacademy.com')
+
+    def test_compare_products_removal(self):
+        driver = self.driver
+        search_field = driver.find_element_by_name('q')
+        search_field.clear()
+        search_field.send_keys('tee')
+        search_field.submit()
+        sleep(2)
+        driver.find_element(By.CLASS_NAME, 'link-compare').click()
+        driver.find_element(By.LINK_TEXT, 'Clear All').click()
+        alert = driver.switch_to.alert()
+        alert.accept()
+
+    def tearDown(self):
+        print('Browser is about to close...')
+        sleep(3)
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity = 2)
